15e9d7002e0add22e9972cdcd42ae04dd469a817
WaghmodeSS <sachinwaghmode1000@gmail.com>
2024-06-21 12:00:12 +0530

    Check the given string is palindrome or not.

--- string.js
+++ string.js
@@ -6,3 +6,16 @@ for (let i = str.length - 1; i >= 0; i--) {
     newStr += str[i];
 }
 console.log(newStr);
+
+// 2 Check the given string is palindrome or not.
+let name$ = 'Dad';
+function checkPalindrome(name$) {
+    let str1 = name$.toLowerCase();
+    let revStr = str1.split('').reverse().join('');
+    if (str1 === revStr) {
+        console.log(name$ + " is a palindrome.");
+    } else {
+        console.log(name$ + " is not a palindrome.");
+    }
+}
+checkPalindrome(name$);
